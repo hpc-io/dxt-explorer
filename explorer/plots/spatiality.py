79ec70532e6c06a1e92a94400452e5ea993cd1a5
Jean Luca Bez <jlbez@lbl.gov>
2025-08-25 11:45:08 -0700

    add default in select

--- explorer/plots/spatiality.py
+++ explorer/plots/spatiality.py
@@ -99,7 +99,7 @@ conditions = [
 
 values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-df["bin"] = np.select(conditions, values)
+df["bin"] = np.select(conditions, values, default='Other')
 
 fig = px.scatter(
     df,
